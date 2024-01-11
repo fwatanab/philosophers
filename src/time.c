797b6c3800551e52c3555b41865bc61f15c41eff
fwatanab <fwatanab@student.42tokyo.jp>
2024-01-11 15:39:08 +0900

    時間のカウントok / 若干の誤差あり

--- src/time.c
+++ src/time.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 04:30:44 by fwatanab          #+#    #+#             */
-/*   Updated: 2024/01/11 13:37:21 by fwatanab         ###   ########.fr       */
+/*   Updated: 2024/01/11 15:36:08 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,6 @@ long long	get_time()
 	return (now_time.tv_sec * 1000LL + now_time.tv_usec / 1000LL);
 }
 
-//void	count_time(t_control *data, int ms_time)
-//{
-//	long long	start_time;
-//	long long	time;
-//
-//	time = 0;
-//	start_time = get_time();
-//	while (time < ms_time)
-//	{
-//		time = get_time() - start_time;
-//		usleep(1000);
-//	}
-//	pthread_mutex_lock(&data->time_mutex);
-//	data->elapsed_time += time;
-//	pthread_mutex_unlock(&data->time_mutex);
-//}
-
 void	count_time(t_control *data, int ms_time)
 {
 	long long	now_time;
@@ -44,10 +27,8 @@ void	count_time(t_control *data, int ms_time)
 	usleep(1000 * ms_time);
 	now_time = get_time();
 //	printf("%lldms\n", now_time - data->start_time);
-	pthread_mutex_lock(&data->time_mutex);
 	data->elapsed_time += (now_time - data->start_time);
 	data->start_time = now_time;
-	pthread_mutex_unlock(&data->time_mutex);
 }
 
 long long	timestamp(struct timeval tv)
