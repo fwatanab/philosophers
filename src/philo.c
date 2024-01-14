213f340cc6c5163f024fe8dd3af3abb5c49816d6
fwatanab <fwatanab@student.42tokyo.jp>
2024-01-14 21:17:05 +0900

    DEL mutex_bet

--- src/philo.c
+++ src/philo.c
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 13:34:43 by fwatanab          #+#    #+#             */
-/*   Updated: 2024/01/11 12:39:47 by fwatanab         ###   ########.fr       */
+/*   Updated: 2024/01/14 21:16:09 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,5 +54,4 @@ void	start_philo(t_control *data)
 	i = 0;
 	while (i < data->number)
 		pthread_mutex_destroy(&data->fork[i++]);
-	pthread_mutex_destroy(&data->bed);
 }
