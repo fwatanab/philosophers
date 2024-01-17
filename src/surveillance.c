1fd08078da0f579530b8c5bc4864c3c3ba8af16c
fwatanab <fwatanab@student.42tokyo.jp>
2024-01-17 16:05:06 +0900

    ADD surveillance

---
+++ src/surveillance.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   surveillance.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 15:55:35 by fwatanab          #+#    #+#             */
+/*   Updated: 2024/01/16 19:35:08 by fwatanab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philosophers.h"
+
+//int	surveillance(t_control *data, t_philo *philo)
+//{
+//	if (data->p_death == DEATH)
+//		return (1);
+//	if ((philo->elapsed_time + data->eat) > data->max_eat)
+//	{
+//		printf("%lld %d died\n",\
+//				philo->elapsed_time, philo->id);
+//		data->p_death = DEATH;
+//		return (1);
+//	}
+//	return (0);
+//}
