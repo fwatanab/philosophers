5eea327a3451aab716bccc14bf5c4a50e915b75f
fwatanab <fwatanab@student.42tokyo.jp>
2024-01-25 21:08:02 +0900

    が時するときもeatingを出力するように修正

--- src/surveillance.c
+++ src/surveillance.c
@@ -6,11 +6,20 @@
 /*   By: fwatanab <fwatanab@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 15:55:35 by fwatanab          #+#    #+#             */
-/*   Updated: 2024/01/24 15:59:36 by fwatanab         ###   ########.fr       */
+/*   Updated: 2024/01/25 21:06:47 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
+
+static void	print_die(t_control *data, t_philo *philo)
+{
+	pthread_mutex_lock(&data->time_mutex);
+	printf("%lld %d died\n", \
+			data->elapsed_time, philo->id);
+	data->p_death = DEATH;
+	pthread_mutex_unlock(&data->time_mutex);
+}
 
 int	surveillance(t_control *data, t_philo *philo)
 {
@@ -26,15 +35,16 @@ int	surveillance(t_control *data, t_philo *philo)
 	pthread_mutex_unlock(&data->time_mutex);
 	if (time > data->die)
 	{
+		pthread_mutex_lock(&philo->control->time_mutex);
+		printf("%lld %d is eating\n", \
+				philo->control->elapsed_time, philo->id);
+		philo->eat_count += 1;
+		pthread_mutex_unlock(&philo->control->time_mutex);
 		pthread_mutex_lock(&data->time_mutex);
 		last_time = data->die - (data->elapsed_time - philo->eat_time);
 		pthread_mutex_unlock(&data->time_mutex);
 		count_time(data, last_time);
-		pthread_mutex_lock(&data->time_mutex);
-		printf("%lld %d died\n", \
-				data->elapsed_time, philo->id);
-		data->p_death = DEATH;
-		pthread_mutex_unlock(&data->time_mutex);
+		print_die(data, philo);
 		return (1);
 	}
 	return (0);
